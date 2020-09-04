2345a3cb99e906d94fa681dbf407faeef4558890
gopishank <66217026+gopishank@users.noreply.github.com>
2020-09-05 03:09:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,14 @@ output "private_subnet_ids" {
   value = [ aws_subnet.public_subnet[*].id ]
 }
 
+// For Expression in Terraform
+output "private_subnets" {
+  value = [
+    for instance in aws_subnet.public_subnet:
+    instance.id
+  ]
+}
+
 // To print the data collected from data sources
 output "aws_availability_zones" {
   value = data.aws_availability_zones.available
@@ -32,12 +40,4 @@ output "public_subnet1" {
 
 output "private_subnet1" {
   value = "${element(aws_subnet.private_subnet.*.id, 1)}"
-}
-
-// For Expression in Terraform
-output "private_subnets" {
-  value = [
-    for instance in aws_subnet.public_subnet:
-    instance.id
-  ]
 }
